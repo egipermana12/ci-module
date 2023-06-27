3b0729dc3d6e0232055885532b50edeb456e1efc
Egi Permana <egi.permana76@gmail.com>
2023-06-27 22:31:48 +0700

    leaflet js
    - menambahkan popup pada leafletjs
    - menambahkan gejson provinsi

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -1,11 +1,16 @@
 <?php
 
 namespace App\Controllers;
+use App\Models\UnitKerjaModel;
+ini_set('max_execution_time', 0); 
+ini_set('memory_limit','2048M');
 
 class Dashboard extends BaseController
 {
+    protected $mUnitkerja;
     public function __construct()
     { 
+        $this->mUnitkerja =  new UnitKerjaModel();
         $this->data['site_title'] = 'Dashboard';
         $this->addStyle(base_url() .'vendors/leaflet/leaflet.css');
         $this->addJs(base_url() .'vendors/leaflet/leaflet.js');
@@ -21,6 +26,30 @@ class Dashboard extends BaseController
         if($currentStatus === 'disable'){
             return $this->printError('anda tidak mempunyai hak akses membuka modul ' . $this->currentModule['judul_module']);   
         }
+        $geoJson = FCPATH . 'geojson/prov.geojson'; // Mendapatkan jalur fisik dari file
+        $file = file_get_contents(realpath($geoJson)); // Membaca isi file
+        $file = json_decode($file);
+        $geoLokasi = $file->features;
+        $this->data['geoLokasi'] = $geoLokasi;
+
+         $coordinates = [
+            [
+                'nm_unit_kerja' => 'Koordinat Default',
+                'alamat' => 'Alamat Default',
+                'koordinat_lokasi' => '-6.248606, 107.117242',
+                'koordinat_bidang' => '',
+                'jarak_toleran' => '5',
+                'gambar' => ''
+            ],
+        ];
+        $markers = $this->mUnitkerja->findAll();
+        if(count($markers) > 0){
+            $this->data['markers'] = $markers;
+        }else{
+            $this->data['markers'] = $coordinates;
+        }
+
+
         return view('Dashboard', $this->data);
     }
 }
--- app/Views/Dashboard.php
+++ app/Views/Dashboard.php
@@ -6,25 +6,58 @@
     <div id="map"></div>
 </div>
 <script type="text/javascript">
-    
+    var data = <?= json_encode($geoLokasi) ?>;
+    var markers = <?= json_encode($markers) ?>;
+
     var customIcon = L.icon({
         iconUrl: '<?= base_url('images/marker-icon.png'); ?>',
-        iconSize:     [42, 42], // size of the icon
-        shadowSize:   [50, 64], // size of the shadow
-        iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-        shadowAnchor: [4, 62],  // the same for the shadow
-        popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+        iconSize: [40, 42],
+        iconAnchor: [24, 41],
+        popupAnchor: [-2, -45],
+        shadowSize: [78, 42],
+        shadowAnchor: [24, 41]
     });
-    var map = L.map("map").setView([-6.282493940225169,107.15327486637672], 16);
-    const tiles = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        maxZoom: 20,
-        id: 'mapbox/streets-v11',
-        tileSize: 512,
-        zoomOffset: -1,
+    function style(feature){
+        return{
+            weight : 2,
+            opacity : 0.5,
+            color : 'orange',
+            dashArray : '3',
+            fillOpacity : 0.1,
+            // fillColor : getColor(parseInt(feature.properties.nilai))
+        };
+    }
+
+    function onEachFeature(feature, layer)
+    {
+        layer.bindPopup("<h4>Provinsi</h4><br>"+feature.properties.Propinsi);
+    }
+
+    var map = L.map("map").setView([-6.248606, 107.117242], 8);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
-    // koordinat.forEach(item => marker = L.marker(item, {icon: customIcon}).bindPopup('Kantor Utama').addTo(map));
-    var marker = L.marker([-6.282493940225169,107.15327486637672], {icon: customIcon}).bindPopup('Kantor Utama').addTo(map)
+
+    //coba gejson
+    var geojson = L.geoJson(data, {
+        style: style,
+        onEachFeature : onEachFeature
+    }).addTo(map);
+
+    var base_url = '<?= base_url('images/unitkerja/') ?>';
+    markers.forEach(function(coordinate) {
+        var latLng = coordinate.koordinat_lokasi.split(',').map(function(coord) {
+            return parseFloat(coord.trim());
+        });
+
+        var imageUrl = base_url + coordinate.gambar;
+        var popupContent = '<h4>' + coordinate.nm_unit_kerja + '</h4>' +
+        '<p>' + coordinate.alamat + '</p>' +
+        '<img src="' + imageUrl + '" alt="Gambar" width="300" height="200" />';
+        L.marker(latLng, {icon: customIcon}).bindPopup(popupContent).addTo(map);
+    });
+
 </script>
     
 <?=$this->endSection()?>
